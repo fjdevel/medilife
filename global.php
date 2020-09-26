faaba1d9510f9beb78a6797bf2061e847c41a93f
unknown <fjdevel@gmail.com>
2020-09-25 18:08:07 -0600

    first commit

---
+++ global.php
@@ -0,0 +1,4 @@
+<?php
+$domain = '/MediLife'
+
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+include_once('./global.php');
+$request = $_SERVER['REQUEST_URI'];
+switch ($request) {
+    case $domain.'/' :
+        require __DIR__ . '/view/home.php';
+        break;
+    case $domain.'' :
+        require __DIR__ . '/view/home.php';
+        break;
+    case $domain.'/login' :
+        require __DIR__ . '/view/login.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/view/404.php';
+        break;
+}
+?>  
